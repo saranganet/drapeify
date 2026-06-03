6b7fd7d95f228367a19b3bf34366af2a0c072e1b
saranganet <soham.saranga@adypu.edu.in>
2026-06-03 15:52:15 +0530

    first commit

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,17 @@ import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 import {defineConfig} from 'vite';
+import sitemap from 'vite-plugin-sitemap';
 
 export default defineConfig(() => {
   return {
-    plugins: [react(), tailwindcss()],
+    plugins: [
+      react(), 
+      tailwindcss(),
+      sitemap({
+        hostname: 'https://drapeify.com'
+      })
+    ],
     resolve: {
       alias: {
         '@': path.resolve(__dirname, '.'),
